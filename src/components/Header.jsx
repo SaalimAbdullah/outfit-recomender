93717fe9afd32e2d9ef19bdbab1f30b86bb7d0e7
Ghayur <154737827+ghayurcodes@users.noreply.github.com>
2025-11-23 00:02:36 +0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import GlitchText from "../UIcomponents/Glitch";
 
-export default function Header(){
-  return (
-    <header className="so-header">
-      <h1>Outfit-Recommender</h1>
-      <p className="muted">Based on: CSP + Backtracking + Heuristics</p>
-    </header>
-  );
+export default function Header() {
+    return (
+        <header className="so-header">
+           
+                <GlitchText
+                    speed={3}
+                    enableShadows={true}
+                    enableOnHover={false}
+                    
+                >
+                    Outfit-Recommender
+                </GlitchText>
+           
+            <p className="muted">Based on: CSP + Backtracking + Heuristics</p>
+        </header>
+    );
 }
